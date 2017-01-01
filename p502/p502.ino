075e750ae00d0ecfa3567192524b4250b0bfc98d
David <David@WILLIAM-PC>
2017-01-01 17:21:49 -0600

    commit pasted folders and files

---
+++ p502/p502.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		p502.ino
+ Created:	1/1/2017 5:15:03 PM
+ Author:	David
+*/
+
+// this was created like any other local project, with no links to repo
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
